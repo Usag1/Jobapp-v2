d7dc9173a4150c913eefd6781d3e43d8cf850b9f
Karin Ikenaga <57020115+Usag1@users.noreply.github.com>
2020-09-24 22:43:33 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import Header from './components/Header';
 import Main from './components/Main';
-import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
+    <div id='home' className='big-bg'>
       <Header />
       <Main />
-      <Footer />
     </div>
   );
 }
