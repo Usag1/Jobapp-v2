37535ae8b843a66d79f2744c9917d5c0c8930eb5
Usag1 <karin_ikenaga@protonmail.com>
2020-10-14 23:56:04 +0900

    Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,8 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faUser} from '@fortawesome/free-solid-svg-icons';
+library.add(faUser)
 
 class Navbar extends Component {
     render() { 
@@ -8,6 +12,7 @@ class Navbar extends Component {
                     <li><a href='App.js'>Home</a></li>
                     <li><a href='About.js'>About</a></li>
                     <li><a href='Contact.js'>Contact</a></li>
+                    <li><a href='#' ><FontAwesomeIcon icon={['fas', 'user']} className='s-logo user' /></a></li>
                 </ul>
             </nav>
          );
